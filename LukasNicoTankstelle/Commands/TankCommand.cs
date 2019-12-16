d541abca34eb7dcf7cf7c6680b132f5077985878
Nico <neppler@online.gibz.ch>
2019-12-16 13:56:21 +0100

    Tanken Command

---
+++ LukasNicoTankstelle/Commands/TankCommand.cs
@@ -0,0 +1,35 @@
+﻿using LukasNicoTankstelle.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LukasNicoTankstelle.Commands
+{
+    public class TankCommand : ICommand
+    {
+        private MainWindow_ViewModel mainViewModel;
+        private PetrolPump_ViewModel petrolPumpViewModel;
+
+        public TankCommand(MainWindow_ViewModel newMainViewModel, PetrolPump_ViewModel newPetrolPumpViewModel)
+        {
+            mainViewModel = newMainViewModel;
+            petrolPumpViewModel = newPetrolPumpViewModel;
+        }
+
+        public void Execute(object parameter)
+        {
+
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+         
+    }
+}
---
+++ LukasNicoTankstelle/ViewModel/PetrolPump_ViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LukasNicoTankstelle.ViewModel
+{
+    public class PetrolPump_ViewModel
+    {
+    }
+}
