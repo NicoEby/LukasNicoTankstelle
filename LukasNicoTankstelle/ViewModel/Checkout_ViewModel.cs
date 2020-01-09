459748c01e6b866716f59e50db673b8ef7867c5d
lussil <lussil@TROMW033603>
2020-01-09 16:53:18 +0100

    added checkout view

---
+++ LukasNicoTankstelle/ViewModel/Checkout_ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using KinoModel.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LukasNicoTankstelle.ViewModel
+{
+    public class Checkout_ViewModel: ModelBase
+    {
+        
+
+    }
+}
